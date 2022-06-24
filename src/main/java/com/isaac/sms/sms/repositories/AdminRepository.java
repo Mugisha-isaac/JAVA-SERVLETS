be89b3860229556ebc352e256d4f41fbd6237f38
didiermun <mugishaisaac2001@gmail.com>
2022-06-24 17:40:25 +0200

    [feat] admin repository is finished

--- src/main/java/com/isaac/sms/sms/repositories/AdminRepository.java
+++ src/main/java/com/isaac/sms/sms/repositories/AdminRepository.java
@@ -42,23 +42,90 @@ public class AdminRepository extends DbConnector {
 
       public Optional<Admin> findById(int id){
           try{
+             String sql = "SELECT id,firstName,lastName,email FROM admins WHERE id=?;";
+             PreparedStatement statement = getConnection().prepareStatement(sql);
+             statement.setInt(1,id);
+              ResultSet resultSet = statement.executeQuery(sql);
+              Admin admin = null;
+              while (resultSet.next()){
+                  admin = new Admin();
+                  admin.setId(resultSet.getInt(1));
+                  admin.setFirstName(resultSet.getString(2));
+                  admin.setLastName(resultSet.getString(3));
+                  admin.setEmail(resultSet.getString(4));
+              }
+
+              if(admin.getFirstName() == null){
+                   return  Optional.empty();
+              }
+
+              return Optional.of(admin);
 
           }
           catch (Exception e){
               System.out.println("Exception" + e.getMessage());
+              return Optional.empty();
           }
-           Optional<Admin> admin  = Optional.of(new Admin());
       }
 
       public Optional<Admin> findByEmail(String email){
-          return null;
+          try{
+              String sql = "SELECT id,firstName,lastName,email FROM admins WHERE email=?;";
+              PreparedStatement statement = getConnection().prepareStatement(sql);
+              statement.setString(1,email);
+              ResultSet resultSet = statement.executeQuery();
+              Admin admin = null;
+              while (resultSet.next()){
+                  admin = new Admin();
+                  admin.setId(resultSet.getInt(1));
+                  admin.setFirstName(resultSet.getString(2));
+                  admin.setLastName(resultSet.getString(3));
+                  admin.setEmail(resultSet.getString(4));
+              }
+
+              if(admin.getFirstName() == null){
+                  return  Optional.empty();
+              }
+
+              return Optional.of(admin);
+
+          }
+          catch (Exception e){
+              System.out.println("Exception" + e.getMessage());
+              return Optional.empty();
+          }
       }
 
       public int deleteById(int id){
-          return null;
+
+          try{
+              String sql = "DELETE FROM admins WHERE id=?;";
+              PreparedStatement statement = getConnection().prepareStatement(sql);
+              statement.setInt(1,id);
+              int count = statement.executeUpdate();
+              return count;
+          }
+          catch (Exception e){
+              System.out.println("Exception" + e.getMessage());
+              return 0;
+          }
       }
 
-      public int updateById(int id){
-          return null;
+      public int updateById(int id, Admin admin){
+          try{
+              String sql = "UPDATE admins SET firstName=?,lastName=?,email=? WHERE id=?;";
+              PreparedStatement statement = getConnection().prepareStatement(sql);
+              statement.setInt(4,id);
+              statement.setString(1,admin.getFirstName());
+              statement.setString(1,admin.getLastName());
+              statement.setString(3,admin.getEmail());
+
+              int updateCount = statement.executeUpdate();
+              return updateCount;
+          }
+          catch (Exception e){
+              System.out.println("Exception" + e.getMessage());
+              return 0;
+          }
       }
 }
